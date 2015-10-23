98db0343d81ceb583bc0508a28f3ccf9c28b81c9
Jackie Becker <jbecker5@stanford.edu>
2015-10-23 00:09:49 -0700

    try number two

---
+++ javascripts/custom.js
@@ -0,0 +1,16 @@
+var smokyBG = $('#wavybg-wrapper').waterpipe({
+    //Default values
+    gradientStart: '#000000',
+    gradientEnd: '#222222',
+    smokeOpacity: 0.1,
+    numCircles: 1,
+    maxMaxRad: 'auto',
+    minMaxRad: 'auto',
+    minRadFactor: 0,
+    iterations: 8,
+    drawsPerFrame: 10,
+    lineWidth: 2,
+    speed: 1,
+    bgColorInner: "#ffffff",
+    bgColorOuter: "#666666"
+});
